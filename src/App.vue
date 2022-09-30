9d17f5cb903d1ad6e58cbbafb7e3149e7fa7cff8
nick <nicholasfoley15@gmail.com>
2022-09-30 14:34:04 +0100

    added functionalities

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <NavBar/>
+    <RouterView/>
+  </div>
+</template>
+
+<script>
+import { RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue';
+
+export default {
+    setup() {
+        return {};
+    },
+    components: { NavBar }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
